4a8d79426a046a4225a737e3cc3fba3804715080
John Mahoney <jmahoney2261@yahoo.com>
2018-05-17 11:39:47 -0400

    Before I mess up the HTML

--- MyPortfolio/Scripts/Factorial.js
+++ MyPortfolio/Scripts/Factorial.js
@@ -1,15 +1,2 @@
 
-        $("#btnFacCalc").click(function () {
-            var facNum = Number($("#factorial").val());
-            var answer = 0;
-            if (facNum > 0 && $("#factorial").val() != "") {
-                for (index = facNum - 1; index > 0; index--) {
-                    facNum = facNum * index;
-                    answer = facNum;
-                }
-                $("#facResult").text("The factorial of the number is " + answer);
-            }
-            else {
-                $("#facResult").text("Make sure there is a value and it's greater then 0");
-            }
-        });
+
