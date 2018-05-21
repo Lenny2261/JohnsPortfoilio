189b111b09d405c29763c2d3b534a897b856806b
John Mahoney <jmahoney2261@yahoo.com>
2018-05-21 19:03:35 -0400

    Working Email WOO

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using MyPortfolio.Models;
 
 namespace MyPortfolio.Controllers
 {
@@ -13,32 +15,36 @@ namespace MyPortfolio.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Excerices()
-        {
-            
-
-            return View();
-        }
-
-        public ActionResult Excerices2()
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
         {
 
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage mM = new MailMessage();
+                    mM.From = new MailAddress("juan226611@yahoo.com");
+                    mM.To.Add("juan226611@yahoo.com");
+                    mM.Subject = model.name + " has sent you an email";
+                    mM.Body = model.message + " test " + model.email;
+                    mM.IsBodyHtml = true;
 
-            return View();
+                    SmtpClient SmtpServer = new SmtpClient();
+
+                    SmtpServer.Credentials = new System.Net.NetworkCredential("juan226611@yahoo.com", "penguins82");
+                    SmtpServer.Port = 587;
+                    SmtpServer.Host = "smtp.mail.yahoo.com";
+                    SmtpServer.EnableSsl = true;
+                    SmtpServer.Send(mM);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("This shouldn't happen");
+                }
+            }
+
+            return View("Index", model);
         }
     }
 }
---
+++ MyPortfolio/Models/ContactModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace MyPortfolio.Models
+{
+    public class ContactModel
+    {
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email:")]
+        public String email { get; set; }
+
+        [Required]
+        //[DataType(DataType.Text)]
+        [Display(Name = "Name:")]
+        public String name { get; set; }
+
+        [Required]
+        //[DataType(DataType.MultilineText)]
+        [Display(Name = "Message:")]
+        public String message { get; set; }
+    }
+}
